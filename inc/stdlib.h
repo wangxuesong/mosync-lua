742498d3eece7b13ad8c536bfed559758d412046
wangxuesong <wangxuesong@gmail.com>
2011-07-31 11:41:34 +0800

    add lua source

---
+++ inc/stdlib.h
@@ -0,0 +1,15 @@
+#ifndef MOSYNC_STDLIB_H
+#define MOSYNC_STDLIB_H
+
+#include <maapi.h>
+#include <maheap.h>
+#include <IX_WIDGET.h>
+#include <GLES/gl.h>
+#include <IX_OPENGL_ES.h>
+
+#define EXIT_FAILURE 1
+#define EXIT_SUCCESS 0
+
+void exit(int result);
+
+#endif
